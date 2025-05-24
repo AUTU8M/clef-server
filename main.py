f830b702c11a086ece809d27a1c42772091295cf
Shubham Sarkar <shubhamsarkar2712@gmail.com>
2025-05-24 15:30:57 +0530

    bcrypt done for pass

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
+import bcrypt
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sqlalchemy import TEXT, VARCHAR, LargeBinary, create_engine, Column
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+import uuid
 
 app = FastAPI()
 
-DATABASE_URL='postgresql://postgres:password123@localhost:5432/clef' 
+#here im doing some testin
+
+DATABASE_URL='postgresql://postgres:admin123@localhost:5432/clef' 
 
 engine = create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(autocommit= False ,autoflush=False, bind= engine)
 
-
+ 
 db = sessionLocal()
 
 class UserCreate(BaseModel):
     name:str
     email:str
     password:str
+
+
+
+#this if for testing 
 
 #creating sqlalchemy declarative base instance
 Base = declarative_base()
@@ -36,9 +44,27 @@ class User(Base):
 @app.post('/signup')
 def signup_user(user: UserCreate):
     #extract the data thats coming from req
-    print(user.name)
-    print(user.email)
-    print(user.password)
+    #print(user.name)
+    #print(user.email)
+    #print(user.password)
+
     #check if the user already exists in db
+
+    user_db = db.query(User).filter(User.email == user.email).first()
+
+    if user_db:
+        return ' User with the same email already exists!'
+
+    hash_pw = bcrypt.hashpw(user.password.encode(),bcrypt.gensalt())
+    user_db = User(id=str(uuid.uuid4()), email=user.email,password=user.password,name=user.name)
+    
+
+    
     #add the user in db
-    pass
+    db.add(user_db)
+    db.commit()
+
+    return user_db
+
+
+Base.metadata.create_all(engine)
